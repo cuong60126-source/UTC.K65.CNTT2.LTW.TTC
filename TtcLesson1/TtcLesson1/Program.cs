5e6262bcf43835a051ff21a2fd1c1d614cbbd481
Cuong Truong <cuong60126@gmail.com>
2026-08-14 18:59:54 +0700

    Add TtcLesson1

---
+++ TtcLesson1/TtcLesson1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TtcLesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
