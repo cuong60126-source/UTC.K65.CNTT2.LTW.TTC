e96a45471c9a01c9d104a584042224e333678b65
Cuong Truong <cuong60126@gmail.com>
2026-08-22 11:06:48 +0700

    Add lesson 2

---
+++ TtcLesson02/TtcLesson02/Program.cs
@@ -0,0 +1,15 @@
+namespace TtcLesson02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello, Truong Tuan Cuong :)");
+
+            app.Run();
+        }
+    }
+}
