41c324301aca9c43a3e06db5a9cc885014e941ec
Cuong Truong <cuong60126@gmail.com>
2026-08-27 21:19:00 +0700

    Add baitap 3

---
+++ TtcLesson03MVC/TtcLesson03MVC/Controllers/TtcController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TtcLesson03MVC.Controllers
+{
+	public class TtcController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View("TtcAbout");
+		}
+	}
+}
