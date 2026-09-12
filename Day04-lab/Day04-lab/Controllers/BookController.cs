42b4130b3b5619d02eaac048e3be0a5999dba6d9
Cuong Truong <cuong60126@gmail.com>
2026-09-12 18:54:20 +0700

    add day04-lab

---
+++ Day04-lab/Day04-lab/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Day04_lab.Models;
+
+namespace Day04_lab.Controllers
+{
+	public class BookController : Controller
+	{
+		protected Book book = new Book();
+		public IActionResult Index()
+		{
+			ViewBag.authors = book.Authors;
+			ViewBag.Genres = book.Genres;
+			var books = book.GetBookList();
+
+			return View(books);
+		}
+	}
+}
---
+++ Day04-lab/Day04-lab/Models/Book.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Day04_lab.Models
+{
+	public class Book
+	{
+		public int id { get; set; }
+		public string title { get; set; }
+		public int authorId { get; set; }
+		public int genreId { get; set; }
+		public string image { get; set; }
+		public float price { get; set; }
+		public int totalPage { get; set; }
+		public string sumary { get; set; }
+		public List<Book> GetBookList()
+		{
+			List<Book> books = new List<Book>()
+			{
+					new Book()
+				{
+					id = 1,
+					title = "Chí Phèo",
+					authorId = 1,
+					genreId = 1,
+					image = "/images/products/b1.jpg",
+					price = 500000,
+					sumary = "",
+					totalPage = 250
+				},
+				new Book()
+				{
+					id = 2,
+					title = "ronaldo",
+					authorId = 2,
+					genreId = 2,
+					image = "/images/products/b2.jpg",
+					price = 2000000,
+					sumary = "",
+					totalPage = 500
+				}
+			};
+
+			return books;
+		}
+		public Book GetBookById(int id)
+		{
+			Book book = this.GetBookList().FirstOrDefault(b => b.id == id);
+			return book;
+		}
+		public List<SelectListItem> Authors { get; } = new List<SelectListItem>
+		{
+			new SelectListItem {Value = "1", Text = "Nam Cao"},
+			new SelectListItem {Value = "2", Text = "Ngô tất tố"}
+		};
+		public List<SelectListItem> Genres { get; } = new List<SelectListItem>
+		{
+			new SelectListItem{Value = "1", Text = "Truyện tranh"},
+			new SelectListItem{Value = "2", Text = "Văn học đương đại"}
+		};
+
+	}
+}
